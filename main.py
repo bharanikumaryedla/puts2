9aea900be3f98e32a9f3b68e1ee3b42c8e73b53c
bharani@bth <bharani@bth.se>
2018-10-16 00:53:17 +0200

    multiplication operation added

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def substraction():
     result=value1-value2
     return '%d \n' % result
 
+@app.route('/mul')
+def multiplication():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1*value2
+    return '%d \n' % result
+
 
 if __name__ == "__main__":
     app.run()
