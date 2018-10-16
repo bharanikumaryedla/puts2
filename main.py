981656c09531f99c34ab5f1122c61073b3e1aa13
bharani@bth <bharani@bth.se>
2018-10-16 23:09:12 +0200

    rational numbers addition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
@@ -32,7 +33,15 @@ def division():
     value1=request.args.get('A',default = 0, type = int)
     value2=request.args.get('B',default = 0, type = int)
     result=float(value1/value2)
-    return '%2f \n' % result
+    return '%.2f \n' % result
+@app.route('/addrational')
+def rational_addition():
+    value1=request.args.get('A',default=0,type=str)
+    numb1,denom1=str(value1).split('/')
+    value2=request.args.get('B',default=0,type=str)
+    numb2,denom2=str(value2).split('/')
+    result=str(Fraction(int(numb1),int(denom1))+Fraction(int(numb2),int(denom2)))
+    return result
 
 if __name__ == "__main__":
     app.run()
